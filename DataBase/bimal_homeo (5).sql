0cb57ff41919cd77263355c1a044eb508f453e25
Robi1996 <robiulislam5024@gmail.com>
2024-04-26 08:46:42 +0530

    updated Code

---
+++ DataBase/bimal_homeo (5).sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2024 at 05:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bimal_homeo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellr`
+--
+
+CREATE TABLE `sellr` (
+  `id` int(10) NOT NULL,
+  `s_Name` varchar(250) DEFAULT NULL,
+  `s_Mob` varchar(10) DEFAULT NULL,
+  `s_Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sellr`
+--
+
+INSERT INTO `sellr` (`id`, `s_Name`, `s_Mob`, `s_Email`) VALUES
+(1, 'Manik Da', '1234569852', 'robiulislam5024@gmail.com'),
+(50, 'Robi', '8617769772', 'robi@gmail.com'),
+(51, 'sajan', '8617769772', 'sajan@gmail.com'),
+(57, 'akram', '8617769772', 'robiulislam5024@gmail.com'),
+(58, 'selim', '8617769772', 'selim@gmail.com'),
+(59, 'rehan', '8617769772', 'rehan@gmail.com'),
+(60, 'ishan', '861776972', 'ishan@gmail.com'),
+(62, 'Robiul islam', '8617769772', 'robiulislam5024@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `username` varchar(10) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Bimal Homeo', 'admin', 'Admin@34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sellr`
+--
+ALTER TABLE `sellr`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sellr`
+--
+ALTER TABLE `sellr`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
